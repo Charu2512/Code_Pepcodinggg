3c1be0ce55c61c0901843b159f8aed14ac153059
Charu2512 <64832473+Charu2512@users.noreply.github.com>
2024-05-25 02:24:13 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+//Pattern
+  
+ 
+
+  
+
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int n=5;
+		for(int i=1;i<=5;i++)
+		{
+		    for(int j=1;j<=i;j++)
+		    {
+		        System.out.print("* ");
+		    }
+		    System.out.print("\n");
+		}
+	
+}
+}
