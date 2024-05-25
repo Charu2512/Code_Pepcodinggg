30e540aca92cbae6f425632a79924a704cddee36
Charu2512 <64832473+Charu2512@users.noreply.github.com>
2024-05-25 20:27:54 +0530

    Create Pattern2.java

---
+++ Pattern2.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+//Pattern
+* * * * * 
+* * * *  
+* * *
+* *
+*
+
+  
+  
+  
+
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int n=5;
+		for(int i=1;i<=5;i++)
+		{
+		    for(int j=n-i;j>=0;j--)
+		    {
+		        System.out.print("* ");
+		    }
+		    System.out.print("\n");
+		}
+	
+}
+}
