f89405c52bdbe46ea08a34ea99a33e0d26c3c9f8
Charu2512 <64832473+Charu2512@users.noreply.github.com>
2024-05-25 22:30:07 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+/*
+ Pattern 9
+
+  
+*       * 
+  *   *   
+    *     
+  *   *   
+*       *  
+  
+  
+  
+  
+*/
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int n=5;
+		for(int i=0;i<n;i++)
+		{
+		    for(int j=0;j<n;j++)
+		    {
+		        if(j==n-1-i || i==j)
+		        System.out.print("* ");
+		        else
+		        System.out.print("  ");
+		    }
+		    System.out.print("\n");
+		}
+	}
+}
