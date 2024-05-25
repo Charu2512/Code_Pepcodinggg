232a5b7a2a4de78435f311b827220e3dffce1ef8
Charu2512 <64832473+Charu2512@users.noreply.github.com>
2024-05-25 22:04:52 +0530

    Create Pattern7.java

---
+++ Pattern7.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+* 
+ * 
+  * 
+   * 
+    * 
+
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		int n=5;
+		int sp=0;
+		for(int i=0;i<n;i++)
+		{
+		    for(int j=0;j<sp;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    System.out.println("* ");
+		    sp++;
+		}
+	}
+}
